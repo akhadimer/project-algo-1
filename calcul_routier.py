224799d5a870b428d619ccbc1668c33af60a6462
Thomas Dumont <thomas.dumont@ynov.com>
2019-10-25 14:16:41 +0200

    Update calcul_routier.py

--- calcul_routier.py
+++ calcul_routier.py
@@ -5,6 +5,9 @@ villes_distances = [['marseille',['toulouse',403],['paris',773],['perpignan',317
                     ['perpignan',['marseille',317],['toulouse',207],['paris',846]],]
 
 def calcul_routier(ville_depart,ville_arrivee):
+    vitesse = 0
+    temps_minutes = 0
+    distance_parcourue = 0
     for pos in range(len(villes_distances)):
         if ville_depart == villes_distances[pos][0]:
             case_ville_dep = pos
@@ -13,14 +16,21 @@ def calcul_routier(ville_depart,ville_arrivee):
         if ville_arrivee == villes_distances[case_ville_dep][pos][0]:
             distance = villes_distances[case_ville_dep][pos][1]
             break
-    print (distance)
+    
+    while (distance_parcourue < distance):
+        if vitesse == 90:
+            temps_minutes += 1
+        if vitesse < 90:
+            vitesse += 10
+            temps_minutes += 1
+        distance_parcourue = distance_parcourue + vitesse / 60
 
-    return ([ville_depart,ville_arrivee])
+    return ([ville_depart,ville_arrivee,distance, temps_minutes])
 
 """
 ville_depart_exist = False
 ville_arrivee_exist = False
-
+print ("Villes disponibles pour le départ et l'arrivée du camion : Marseille, Toulouse, Paris, Perpignan")
 while True:
     ville_depart = input('Entrez la ville de départ : ').lower()
     for position in range(len(villes_existantes)):
@@ -46,7 +56,6 @@ while True:
     if ville_arrivee_exist == True:
         break
 """
-tab = []
 tab = calcul_routier('marseille','toulouse')
 
-#print (tab)
+print (tab)
